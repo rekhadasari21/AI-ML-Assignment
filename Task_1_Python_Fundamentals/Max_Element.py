dce131982a2b79c6e42e23ecc6cae826574c53fd
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 16:42:51 +0530

    Find maximum element in a list

--- Task_1_Python_Fundamentals/Max_Element.py
+++ Task_1_Python_Fundamentals/Max_Element.py
@@ -1,7 +1,7 @@
-#Find maximum element in a list
+# Find maximum element in a list
 def find_max(numbers):
   max_val = numbers[0]
   for num in numbers:
-    if num > max_val:
-      max_val = num
+    if num>max_val:
+      max_val=num
   return max_val
