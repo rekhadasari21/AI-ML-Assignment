1c540a35144239d3f3d4c9275bb82f2c236e1341
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 16:46:15 +0530

    Remove duplicates without using set()

--- Task_1_Python_Fundamentals/remove_duplicates_without_set.py
+++ Task_1_Python_Fundamentals/remove_duplicates_without_set.py
@@ -1,6 +1,6 @@
 # Remove duplicates without using set()
 def remove_duplicates(items):
-    unique_items = []
+    unique_items=[]
     for item in items:
         if item not in unique_items:
             unique_items.append(item)
