8d77accaaebb517859749777e88e36edfb7b369b
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 16:07:06 +0530

    Create word_frequency_paragraph.py

---
+++ Task_1_Python_Fundamentals/word_frequency_paragraph.py
@@ -0,0 +1,10 @@
+#word frequency in a paragraph
+def word_frequency(text):
+    frequency = {}
+    words = text.lower().split()
+    for word in words:
+        if word in frequency:
+            frequency[word] += 1
+        else:
+            frequency[word] = 1
+    return frequency
