fe778d7f305e2110c9a72a6cfff6594ca4031287
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 16:43:31 +0530

    Reverse a string without using reverse()

--- Task_1_Python_Fundamentals/Reverse_string.py
+++ Task_1_Python_Fundamentals/Reverse_string.py
@@ -1,6 +1,6 @@
 # Reverse a string without using reverse()
 def reverse_string(text):
-    reversed_text = ""
+    reversed_text =""
     for char in text:
-        reversed_text = char + reversed_text
+        reversed_text =char + reversed_text
     return reversed_text
