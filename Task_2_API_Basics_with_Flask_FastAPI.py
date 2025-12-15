66ad721255957a7e85a41edbd1703dd73c559a4c
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 17:22:21 +0530

    API_Basics_with_Flask_FastAPI.py

--- Task_2_API_Basics_with_Flask_FastAPI.py
+++ Task_2_API_Basics_with_Flask_FastAPI.py
@@ -32,9 +32,9 @@ def square_number():
             "result": result, 
             "status": "success"
         }),200
-
+        
     except Exception as e:
         return jsonify({"error": str(e)}),500
 
-if __name__ == '__main__':
+if __name__ =='__main__':
     app.run(debug=True, port=5000)
