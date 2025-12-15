b4c107f1364596e3cc88720560aedd332cc1ff96
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 16:45:11 +0530

    Check if a number is prime

--- Task_1_Python_Fundamentals/check_prime_number.py
+++ Task_1_Python_Fundamentals/check_prime_number.py
@@ -1,8 +1,8 @@
 # Check if a number is prime
 def is_prime(number):
-    if number <= 1:
+    if number<=1:
         return False
-    for i in range(2, int(number ** 0.5) + 1):
-        if number % i == 0:
+    for i in range(2,int(number ** 0.5)+1):
+        if number%i==0:
             return False
     return True
