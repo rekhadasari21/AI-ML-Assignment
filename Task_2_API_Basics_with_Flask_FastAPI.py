28ef479c776f0f1264a5b739592252fa6f517cab
rekhadasari21 <rekha06yadav26@gmail.com>
2025-12-15 17:20:21 +0530

    Update and rename Task_2_API_Basics_with_Flask_FastAPI to Task_2_API_Basics_with_Flask_FastAPI.py

--- Task_2_API_Basics_with_Flask_FastAPI.py
+++ Task_2_API_Basics_with_Flask_FastAPI.py
@@ -1,40 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
 # GET API Endpoint: Welcome Message
-@app.route('/', methods=['GET'])
+@app.route('/',methods=['GET'])
 def home():
     return jsonify({
-        "message": "Welcome to the AI/ML Internship API", 
-        "status": "success"
-    }), 200
+        "message": "Welcome to AIML Internship API", 
+        "status":"success"
+    }),200
 
 # POST API Endpoint: Square a number
-@app.route('/square', methods=['POST'])
+@app.route('/square',methods=['POST'])
 def square_number():
     try:
-        data = request.get_json()
+        data=request.get_json()
         
-        # Error Handling: Check if input exists
+        # Error Handling:Check if input exists
         if not data or 'number' not in data:
-            return jsonify({"error": "Missing 'number' field"}), 400
+            return jsonify({"error": "Missing 'number' field"}),400
             
         number = data['number']
         
         # Error Handling: Validate data type
         if not isinstance(number, (int, float)):
-            return jsonify({"error": "Input must be a number"}), 400
+            return jsonify({"error": "Input must be a number"}),400
 
-        result = number ** 2
+        result=number**2
         return jsonify({
             "input": number, 
             "result": result, 
             "status": "success"
-        }), 200
+        }),200
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": str(e)}),500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
